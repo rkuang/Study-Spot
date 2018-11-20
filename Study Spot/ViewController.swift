b1e6570cd7fa95e224cb62b2479f0c8233119b14
Ricky Kuang <rkuang26@gmail.com>
2018-11-19 16:39:05 -0800

    Initial Commit

---
+++ Study Spot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study Spot
+//
+//  Created by Ricky Kuang on 11/19/18.
+//  Copyright © 2018 Ricky Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
