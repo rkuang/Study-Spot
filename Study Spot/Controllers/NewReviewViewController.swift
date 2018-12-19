171434040dc71be493295713b46654cba438c953
Ricky Kuang <rkuang26@gmail.com>
2018-12-18 19:47:19 -0800

    new review design complete. just need to wire everything up

--- Study Spot/Controllers/NewReviewViewController.swift
+++ Study Spot/Controllers/NewReviewViewController.swift
@@ -16,28 +16,58 @@ class NewReviewViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var ratingSlider: CosmosView!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var hintLabel: UILabel!
-    @IBOutlet weak var collectionView: UICollectionView!
+
+    var textViewFrame: CGRect!
+    var keyboardHeight: CGFloat!
     
     func setupViews() {
         titleLabel.text = titleLabelText
+        
         textView.delegate = self
         textView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         textView.textContainer.lineFragmentPadding = 0
+        textView.addDoneButtonOnKeyboard()
     }
     
     // MARK: - UITextViewDelegate
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textViewFrame = textView.frame
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            print("did begin editing")
+            self.textView.frame = CGRect(x: self.textView.frame.minX, y: 24, width: self.textView.frame.width, height: self.view.frame.height - self.keyboardHeight)
+            self.hintLabel.frame = CGRect(x: self.textView.frame.minX, y: 24, width: self.hintLabel.frame.width, height: self.hintLabel.frame.height)
+        }, completion: nil)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            print("did end editing")
+            self.textView.frame = self.textViewFrame
+            self.hintLabel.frame = CGRect(x: self.textView.frame.minX, y: self.textView.frame.minY, width: self.hintLabel.frame.width, height: self.hintLabel.frame.height)
+        }, completion: nil)
+    }
     
     func textViewDidChange(_ textView: UITextView) {
         if textView.text.count > 0 {
             hintLabel.isHidden = true
         } else {
             hintLabel.isHidden = false
+        }
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRect = keyboardFrame.cgRectValue
+            print(keyboardRect.minY)
+            keyboardHeight = keyboardRect.height + 24 + 16
         }
     }
     
@@ -50,5 +80,25 @@ class NewReviewViewController: UIViewController, UITextViewDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+}
 
+extension UITextView {
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonAction))
+        
+        doneToolbar.items = [flexSpace, done]
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
 }
